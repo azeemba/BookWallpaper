a5ef9b7d36290a7a30a7cda17c38bafa38aa869b
Azeem Bande-Ali <me@azeemba.com>
2024-08-31 22:02:42 -0400

    Add progress bars while waiting on internet

--- app/src/main/java/com/azeemba/bookwallpaper/MainActivity.kt
+++ app/src/main/java/com/azeemba/bookwallpaper/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.azeemba.bookwallpaper
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.HIDE_IMPLICIT_ONLY
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var bookAdapter: BookAdapter
 
+    private lateinit var progressBarr: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         bookAuthor = findViewById(R.id.bookAuthor)
         searchButton = findViewById(R.id.searchButton)
         recyclerView = findViewById(R.id.recyclerView)
+        progressBarr = findViewById(R.id.progressBar_cyclic)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
 
@@ -33,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = bookAdapter
 
         searchButton.setOnClickListener {
+            progressBarr.visibility = View.VISIBLE
             val title = bookTitle.text.toString()
             val author = bookAuthor.text.toString()
             val query = "intitle:$title+inauthor:$author"
@@ -43,10 +52,12 @@ class MainActivity : AppCompatActivity() {
                         val books = response.body()?.items ?: emptyList()
                         bookAdapter.submitList(books)
                     }
+                    progressBarr.visibility = View.GONE
                 }
 
                 override fun onFailure(call: Call<BookSearchResponse>, t: Throwable) {
                     // Handle error
+                    progressBarr.visibility = View.GONE
                 }
             })
         }
--- app/src/main/java/com/azeemba/bookwallpaper/WallpaperPreviewActivity.kt
+++ app/src/main/java/com/azeemba/bookwallpaper/WallpaperPreviewActivity.kt
@@ -2,24 +2,32 @@ package com.azeemba.bookwallpaper
 
 import android.app.WallpaperManager
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 
 class WallpaperPreviewActivity : AppCompatActivity() {
 
     private lateinit var previewImageView: ImageView
     private lateinit var confirmButton: Button
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +35,14 @@ class WallpaperPreviewActivity : AppCompatActivity() {
 
         previewImageView = findViewById(R.id.previewImageView)
         confirmButton = findViewById(R.id.confirmButton)
+        progressBar = findViewById(R.id.progressBar_wp)
 
         val bookId = intent.getStringExtra("BOOK_ID")
         val bookTitle = intent.getStringExtra("BOOK_TITLE")
         var imageUrl = ""
 
         if (bookId != null) {
+            progressBar.visibility = View.VISIBLE
             ApiServiceProvider.bookApiService.getVolumeInfo(bookId, ApiServiceProvider.API_KEY).enqueue(object: Callback<VolumeInfo>{
                 override fun onResponse(call: Call<VolumeInfo>, response: Response<VolumeInfo>) {
                     if (response.isSuccessful) {
@@ -43,6 +53,28 @@ class WallpaperPreviewActivity : AppCompatActivity() {
 
                         Glide.with(baseContext)
                             .load(imageUrl)
+                            .listener(object: RequestListener<Drawable> {
+                                override fun onLoadFailed(
+                                    e: GlideException?,
+                                    model: Any?,
+                                    target: Target<Drawable>?,
+                                    isFirstResource: Boolean
+                                ): Boolean {
+                                    progressBar.visibility = View.GONE
+                                    return false
+                                }
+
+                                override fun onResourceReady(
+                                    resource: Drawable?,
+                                    model: Any?,
+                                    target: Target<Drawable>?,
+                                    dataSource: DataSource?,
+                                    isFirstResource: Boolean
+                                ): Boolean {
+                                    progressBar.visibility = View.GONE
+                                    return false
+                                }
+                            })
                             .into(previewImageView)
 
                         confirmButton.setOnClickListener {
@@ -60,6 +92,7 @@ class WallpaperPreviewActivity : AppCompatActivity() {
     }
 
     private fun setWallpaper(imageUrl: String?) {
+        progressBar.visibility = View.VISIBLE
         Glide.with(this)
             .asBitmap()
             .load(imageUrl)
@@ -70,10 +103,12 @@ class WallpaperPreviewActivity : AppCompatActivity() {
                         wallpaperManager.setBitmap(resource, null, true, WallpaperManager.FLAG_LOCK)
                         wallpaperManager.setBitmap(resource, null, true, WallpaperManager.FLAG_SYSTEM)
                         Toast.makeText(this@WallpaperPreviewActivity, "Wallpaper set successfully", Toast.LENGTH_SHORT).show()
+                        progressBar.visibility = View.GONE
                         finish()
                         moveTaskToBack(true)
                     } catch (e: Exception) {
                         Toast.makeText(this@WallpaperPreviewActivity, "Failed to set wallpaper", Toast.LENGTH_SHORT).show()
+                        progressBar.visibility = View.GONE
                     }
                 }
             })
